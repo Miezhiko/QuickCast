f8146a9d5725927f8ce4c2913948337927d71697
Miezhiko <Miezhiko@gmail.com>
2022-04-29 22:11:30 +0400

    simple
    Signed-off-by: Miezhiko <Miezhiko@gmail.com>

---
+++ README.md
@@ -0,0 +1,7 @@
+# quickcast
+
+[![Discord](https://img.shields.io/discord/611822838831251466?label=Discord&color=pink)](https://discord.gg/GdzjVvD)
+[![Twitter Follow](https://img.shields.io/twitter/follow/Miezhiko.svg?style=social)](https://twitter.com/Miezhiko)
+
+ - sets quick cast on some buttons
+ - blocks scroll
---
+++ main.cxx
@@ -0,0 +1,53 @@
+#include <Windows.h>
+
+HHOOK kh, mh;
+
+void click() {
+  INPUT Inputs[2] = {0};
+  Inputs[0].type = INPUT_MOUSE;
+  Inputs[0].mi.dwFlags = MOUSEEVENTF_LEFTDOWN;
+
+  Inputs[1].type = INPUT_MOUSE;
+  Inputs[1].mi.dwFlags = MOUSEEVENTF_LEFTUP;
+
+  SendInput(3, Inputs, sizeof(INPUT));
+}
+
+LRESULT __stdcall KeyboardCallback(int c, WPARAM p, LPARAM l) {
+  if (c == HC_ACTION) {
+    switch(p) {
+      case WM_KEYUP:
+      case WM_SYSKEYUP: {
+        PKBDLLHOOKSTRUCT btn = (PKBDLLHOOKSTRUCT)l;
+         if (btn->vkCode == 0x5A) { // Z
+          click();
+        }
+      } break;
+      default: break;
+    }
+  }
+  return CallNextHookEx(kh, c, p, l);
+}
+
+LRESULT __stdcall MouseCallback(int c, WPARAM p, LPARAM l) {
+  if (c == HC_ACTION) {
+    switch(p) {
+      case WM_MOUSEWHEEL: return 1;
+      default: break;
+    }
+  }
+  return CallNextHookEx(mh, c, p, l);
+}
+
+int WINAPI WinMain(HINSTANCE i, HINSTANCE pi, LPSTR l, int s) {
+  kh = SetWindowsHookEx(WH_KEYBOARD_LL, KeyboardCallback, NULL, 0);
+  mh = SetWindowsHookEx(WH_MOUSE_LL, MouseCallback, NULL, 0);
+  MSG msg;
+  while (GetMessage(&msg, NULL, 0, 0)) {
+    TranslateMessage(&msg);
+    DispatchMessage(&msg);
+  }
+  UnhookWindowsHookEx(mh);
+  UnhookWindowsHookEx(kh);
+  return static_cast<int>(msg.wParam);
+}
