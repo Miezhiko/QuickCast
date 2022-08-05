f95755304ce4cbf09e0e38c54850085de229ed7a
Miezhiko <Miezhiko@gmail.com>
2022-08-05 18:03:18 +0400

    config file support with unlimited (limited) keys
    Signed-off-by: Miezhiko <Miezhiko@gmail.com>

--- README.md
+++ README.md
@@ -3,10 +3,40 @@
 [![Discord](https://img.shields.io/discord/611822838831251466?label=Discord&color=pink)](https://discord.gg/GdzjVvD)
 [![Twitter Follow](https://img.shields.io/twitter/follow/Miezhiko.svg?style=social)](https://twitter.com/Miezhiko)
 
-work in progress
-----------------
+TLDR
+----
 
-at the moment it's more like an experiment
+Quick cast is automatically pressing left mouse when you press hotkey, just like in Dota/LOL/Hots
+
+
+Configuration
+-------------
+
+you need to modify conf.ini file
+
+```ini
+# you can add more keys here and change their values
+# (key names are ignored, current ones are A and Z)
+[KEYS]
+ATTACK          = 0x41
+MOVE            = 0x5A
+
+# this is to avoid clicking on menus
+# change BORDERS_CHECK to 0 to disable checks
+[BORDERS]
+BORDERS_CHECK   = 1
+MENU_HEIGHT     = 75
+GAME_HEIGHT     = 1100
+GAME_HEIGHT2    = 1000
+MINIMAP_WIDTH   = 695
+MINIMAP_WIDTH2  = 800
+GAME_MID_WIDTH  = 1750
+```
+
+Story
+-----
+
+Warcraft 3 Reforged has official support for this but only for their `GRID` hotkeys setup, so I consider that this is not a hack, it's just handy workaround over bad game design at current state. For people who play mobas it's really painful that game doesn't give you this out from the box.
 
 global hook thing
 -----------------
@@ -15,7 +45,6 @@ global hook thing
  - register some hotkeys for quickcast
  - `Scroll Lock` indicates that quick cast is enabled
  - click `Scroll Lock` to enable or disable hotkeys
- - current quickcast hotkeys are: `Z` and `A`
  - controls screen borders (menu, bottom menu, minimap)
  - blocks prscrn, win key etc...
  - use Ctrl+Backspace to exit
--- global_hook.c
+++ global_hook.c
@@ -1,32 +1,43 @@
 #define WIN32_LEAN_AND_MEAN
 #include <Windows.h>
+#include <stdlib.h>
 
-#define MENU_HEIGHT     75
-#define GAME_HEIGHT     1100
-#define GAME_HEIGHT2    1000
-#define MINIMAP_WIDTH   695
-#define MINIMAP_WIDTH2  800
-#define GAME_MID_WIDTH  1750
+#define INI_FILE_MAX        32767
 
-#define MOVE_KEY    0x5A
-#define ATTACK_KEY  0x41
-#define TOGGLE_KEY  VK_SCROLL
-#define EXIT_KEY    VK_BACK
+#define TOGGLE_KEY          VK_SCROLL
+#define EXIT_KEY            VK_BACK
 
-const CHAR MUTEX_NAME[10] = "quickcast";
-const INT SIZE_OF_INPUT = sizeof(INPUT);
+static BOOL BORDERS_CHECK   = TRUE;
+static LONG MENU_HEIGHT     = 75;
+static LONG GAME_HEIGHT     = 1100;
+static LONG GAME_HEIGHT2    = 1000;
+static LONG MINIMAP_WIDTH   = 695;
+static LONG MINIMAP_WIDTH2  = 800;
+static LONG GAME_MID_WIDTH  = 1750;
+
+const CHAR* CONF_FILE       = "../../conf.ini";
+
+const CHAR* BORDERS_SECTION = "BORDERS";
+const CHAR* KEYS_SECTION    = "KEYS";
+
+const CHAR* MUTEX_NAME      = "quickcast";
+
+static BOOL HOTKEYS_ON      = TRUE;
+const INT SIZE_OF_INPUT     = sizeof(INPUT);
+
+DWORD* CONFIG_KEYS          = NULL;
+INT CONFIG_KEYS_SIZE        = 0;
+INT CONFIG_KEYS_ITERATOR    = 0;
 
 static INPUT INPUT_DOWN;
 static INPUT INPUT_UP;
 
 static HHOOK KEYBOARD_HOOK;
 
-static BOOL HOTKEYS_ON = TRUE;
-
 static POINT CURSOR_POSITION;
 
 inline VOID mouseLeftClick() {
-  if (GetCursorPos(&CURSOR_POSITION)) {
+  if (BORDERS_CHECK && GetCursorPos(&CURSOR_POSITION)) {
     if (CURSOR_POSITION.y < MENU_HEIGHT) {
       SetCursorPos(CURSOR_POSITION.x, MENU_HEIGHT);
     }
@@ -43,17 +54,19 @@ inline VOID mouseLeftClick() {
         }
       }
     }
-    SendInput(1, &INPUT_DOWN, SIZE_OF_INPUT);
-    SendInput(1, &INPUT_UP, SIZE_OF_INPUT);
   }
+  SendInput(1, &INPUT_DOWN, SIZE_OF_INPUT);
+  SendInput(1, &INPUT_UP, SIZE_OF_INPUT);
 }
 
 inline VOID processHotkeys(KBDLLHOOKSTRUCT *kbd) {
   switch (kbd->vkCode) {
+    /*
     case MOVE_KEY:
     case ATTACK_KEY:
       if (HOTKEYS_ON) mouseLeftClick();
       break;
+    */
     case TOGGLE_KEY:
       HOTKEYS_ON = !HOTKEYS_ON;
       break;
@@ -62,6 +75,15 @@ inline VOID processHotkeys(KBDLLHOOKSTRUCT *kbd) {
         PostQuitMessage(0);
       break;
     default:
+      if (HOTKEYS_ON)
+      for( CONFIG_KEYS_ITERATOR = 0
+         ; CONFIG_KEYS_ITERATOR < CONFIG_KEYS_SIZE
+         ; ++CONFIG_KEYS_ITERATOR ) {
+        if (kbd->vkCode == CONFIG_KEYS[CONFIG_KEYS_ITERATOR]) {
+          mouseLeftClick();
+          break;
+        }
+      }
       break;
   }
 }
@@ -93,10 +115,53 @@ LRESULT CALLBACK KeyboardCallback( INT uMsg
   return CallNextHookEx(KEYBOARD_HOOK, uMsg, wParam, lParam);
 }
 
+inline VOID parseConfig() {
+  CHAR keys[INI_FILE_MAX];
+  CHAR *currentStr;
+  CHAR *context;
+  CHAR *valueStr;
+  DWORD value;
+  INT len;
+  GetPrivateProfileSectionA(KEYS_SECTION, keys, INI_FILE_MAX, CONF_FILE);
+  // calculate array lenght
+  CHAR *current = keys;
+  while (*current) {
+    len = strlen(current);
+    currentStr = strtok_s(current, "=", &context);
+    ++CONFIG_KEYS_SIZE;
+    current += len + 1;
+  }
+  CONFIG_KEYS = malloc(CONFIG_KEYS_SIZE * sizeof(DWORD));
+  CONFIG_KEYS_SIZE = 0;
+  len = 0;
+  current = keys;
+  // fill config keys array
+  while (*current) {
+    len = strlen(current);
+    currentStr = strtok_s(current, "=", &context);
+    valueStr = (currentStr + strlen(currentStr) + 1);
+    value = strtoul(valueStr, NULL, 16);
+    CONFIG_KEYS[CONFIG_KEYS_SIZE] = value;
+    ++CONFIG_KEYS_SIZE;
+    current += len + 1;
+  }
+
+  BORDERS_CHECK = GetPrivateProfileInt(BORDERS_SECTION, "BORDERS_CHECK", BORDERS_CHECK, CONF_FILE);
+  if (BORDERS_CHECK) {
+    MENU_HEIGHT     = GetPrivateProfileInt(BORDERS_SECTION, "MENU_HEIGHT", MENU_HEIGHT, CONF_FILE);
+    GAME_HEIGHT     = GetPrivateProfileInt(BORDERS_SECTION, "GAME_HEIGHT", GAME_HEIGHT, CONF_FILE);
+    GAME_HEIGHT2    = GetPrivateProfileInt(BORDERS_SECTION, "GAME_HEIGHT2", GAME_HEIGHT2, CONF_FILE);
+    MINIMAP_WIDTH   = GetPrivateProfileInt(BORDERS_SECTION, "MINIMAP_WIDTH", MINIMAP_WIDTH, CONF_FILE);
+    MINIMAP_WIDTH2  = GetPrivateProfileInt(BORDERS_SECTION, "MINIMAP_WIDTH2", MINIMAP_WIDTH2, CONF_FILE);
+    GAME_MID_WIDTH  = GetPrivateProfileInt(BORDERS_SECTION, "GAME_MID_WIDTH", GAME_MID_WIDTH, CONF_FILE);
+  }
+}
+
 INT WINAPI WinMain( HINSTANCE hInstance
                   , HINSTANCE hPrevInstance
                   , LPSTR lpCmdLine
                   , int nShowCmd ) {
+  parseConfig();
 
   // Turn on Num Lock
   if (!(GetKeyState(VK_NUMLOCK) & 0x0001)) {
