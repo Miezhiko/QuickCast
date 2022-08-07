e31740b8dffa5a8152f6c27ad4d19b47cc65d2b1
Miezhiko <Miezhiko@gmail.com>
2022-08-07 11:40:22 +0400

    various important optimizations!
    Signed-off-by: Miezhiko <Miezhiko@gmail.com>

--- global_hook.c
+++ global_hook.c
@@ -15,17 +15,15 @@ static LONG MINIMAP_WIDTH   = 695;
 static LONG MINIMAP_WIDTH2  = 800;
 static LONG GAME_MID_WIDTH  = 1750;
 
-const CHAR* CONF_FILE       = "../../conf.ini";
-
-const CHAR* BORDERS_SECTION = "BORDERS";
-const CHAR* KEYS_SECTION    = "KEYS";
-
-const CHAR* MUTEX_NAME      = "quickcast";
+const CHAR *CONF_FILE       = "../../conf.ini";
+const CHAR *BORDERS_SECTION = "BORDERS";
+const CHAR *KEYS_SECTION    = "KEYS";
+const CHAR *MUTEX_NAME      = "quickcast";
 
 static BOOL HOTKEYS_ON      = TRUE;
 const INT SIZE_OF_INPUT     = sizeof(INPUT);
 
-DWORD* CONFIG_KEYS          = NULL;
+DWORD *CONFIG_KEYS          = NULL;
 INT CONFIG_KEYS_SIZE        = 0;
 INT CONFIG_KEYS_ITERATOR    = 0;
 
@@ -36,12 +34,11 @@ static HHOOK KEYBOARD_HOOK;
 
 static POINT CURSOR_POSITION;
 
-inline VOID mouseLeftClick() {
+inline VOID bordersCheck() {
   if (BORDERS_CHECK && GetCursorPos(&CURSOR_POSITION)) {
     if (CURSOR_POSITION.y < MENU_HEIGHT) {
       SetCursorPos(CURSOR_POSITION.x, MENU_HEIGHT);
-    }
-    if (CURSOR_POSITION.y > GAME_HEIGHT2) {
+    } else if (CURSOR_POSITION.y > GAME_HEIGHT2) {
       if (CURSOR_POSITION.x > GAME_MID_WIDTH) {
         SetCursorPos(CURSOR_POSITION.x, GAME_HEIGHT2);
       } else if (CURSOR_POSITION.x > MINIMAP_WIDTH) {
@@ -55,12 +52,16 @@ inline VOID mouseLeftClick() {
       }
     }
   }
+}
+
+inline VOID mouseLeftClick() {
+  bordersCheck();
   SendInput(1, &INPUT_DOWN, SIZE_OF_INPUT);
   SendInput(1, &INPUT_UP, SIZE_OF_INPUT);
 }
 
-inline VOID processHotkeys(KBDLLHOOKSTRUCT *kbd) {
-  switch (kbd->vkCode) {
+inline VOID processHotkeys(DWORD code) {
+  switch (code) {
     case TOGGLE_KEY:
       HOTKEYS_ON = !HOTKEYS_ON;
       break;
@@ -73,7 +74,7 @@ inline VOID processHotkeys(KBDLLHOOKSTRUCT *kbd) {
       for( CONFIG_KEYS_ITERATOR = 0
          ; CONFIG_KEYS_ITERATOR < CONFIG_KEYS_SIZE
          ; ++CONFIG_KEYS_ITERATOR ) {
-        if (kbd->vkCode == CONFIG_KEYS[CONFIG_KEYS_ITERATOR]) {
+        if (code == CONFIG_KEYS[CONFIG_KEYS_ITERATOR]) {
           mouseLeftClick();
           break;
         }
@@ -88,8 +89,7 @@ LRESULT CALLBACK KeyboardCallback( INT uMsg
 
   if (uMsg == HC_ACTION) {
     switch(wParam) {
-      case WM_KEYDOWN:
-      case WM_SYSKEYDOWN: {
+      case WM_KEYDOWN: {
         switch ( ((KBDLLHOOKSTRUCT*)lParam)->vkCode )  {
           case VK_SNAPSHOT:
           case VK_LWIN:
@@ -99,9 +99,8 @@ LRESULT CALLBACK KeyboardCallback( INT uMsg
         }
         break;
       }
-      case WM_KEYUP:
-      case WM_SYSKEYUP: {
-        processHotkeys( (KBDLLHOOKSTRUCT*)lParam );
+      case WM_KEYUP: {
+        processHotkeys( ((KBDLLHOOKSTRUCT*)lParam)->vkCode );
       } break;
       default: break;
     }
@@ -109,19 +108,19 @@ LRESULT CALLBACK KeyboardCallback( INT uMsg
   return CallNextHookEx(KEYBOARD_HOOK, uMsg, wParam, lParam);
 }
 
-inline VOID parseConfig() {
-  CHAR keys[INI_FILE_MAX];
-  CHAR *currentStr;
-  CHAR *context;
-  CHAR *valueStr;
-  DWORD value;
-  INT len;
+inline VOID parseConfigFile() {
+  DWORD value       = 0;
+  INT len           = 0;
+  CHAR *currentStr  = NULL;
+  CHAR *context     = NULL;
+  CHAR* valueStr    = NULL;
+  // don't use stack fo large array
+  CHAR *keys = malloc(INI_FILE_MAX * sizeof(CHAR));
   GetPrivateProfileSectionA(KEYS_SECTION, keys, INI_FILE_MAX, CONF_FILE);
   // calculate array lenght
   CHAR *current;
   for(current = keys; *current; ++current)
     CONFIG_KEYS_SIZE += *current == '\n';
-  const char *str;
   CONFIG_KEYS = malloc(CONFIG_KEYS_SIZE * sizeof(DWORD));
   CONFIG_KEYS_SIZE = 0;
   len = 0;
@@ -137,22 +136,24 @@ inline VOID parseConfig() {
     current += len + 1;
   }
 
-  BORDERS_CHECK = GetPrivateProfileInt(BORDERS_SECTION, "BORDERS_CHECK", BORDERS_CHECK, CONF_FILE);
+  BORDERS_CHECK = GetPrivateProfileIntA(BORDERS_SECTION, "BORDERS_CHECK", BORDERS_CHECK, CONF_FILE);
   if (BORDERS_CHECK) {
-    MENU_HEIGHT     = GetPrivateProfileInt(BORDERS_SECTION, "MENU_HEIGHT", MENU_HEIGHT, CONF_FILE);
-    GAME_HEIGHT     = GetPrivateProfileInt(BORDERS_SECTION, "GAME_HEIGHT", GAME_HEIGHT, CONF_FILE);
-    GAME_HEIGHT2    = GetPrivateProfileInt(BORDERS_SECTION, "GAME_HEIGHT2", GAME_HEIGHT2, CONF_FILE);
-    MINIMAP_WIDTH   = GetPrivateProfileInt(BORDERS_SECTION, "MINIMAP_WIDTH", MINIMAP_WIDTH, CONF_FILE);
-    MINIMAP_WIDTH2  = GetPrivateProfileInt(BORDERS_SECTION, "MINIMAP_WIDTH2", MINIMAP_WIDTH2, CONF_FILE);
-    GAME_MID_WIDTH  = GetPrivateProfileInt(BORDERS_SECTION, "GAME_MID_WIDTH", GAME_MID_WIDTH, CONF_FILE);
+    MENU_HEIGHT     = GetPrivateProfileIntA(BORDERS_SECTION, "MENU_HEIGHT", MENU_HEIGHT, CONF_FILE);
+    GAME_HEIGHT     = GetPrivateProfileIntA(BORDERS_SECTION, "GAME_HEIGHT", GAME_HEIGHT, CONF_FILE);
+    GAME_HEIGHT2    = GetPrivateProfileIntA(BORDERS_SECTION, "GAME_HEIGHT2", GAME_HEIGHT2, CONF_FILE);
+    MINIMAP_WIDTH   = GetPrivateProfileIntA(BORDERS_SECTION, "MINIMAP_WIDTH", MINIMAP_WIDTH, CONF_FILE);
+    MINIMAP_WIDTH2  = GetPrivateProfileIntA(BORDERS_SECTION, "MINIMAP_WIDTH2", MINIMAP_WIDTH2, CONF_FILE);
+    GAME_MID_WIDTH  = GetPrivateProfileIntA(BORDERS_SECTION, "GAME_MID_WIDTH", GAME_MID_WIDTH, CONF_FILE);
   }
+  free(keys);
 }
 
-INT WINAPI WinMain( HINSTANCE hInstance
-                  , HINSTANCE hPrevInstance
-                  , LPSTR lpCmdLine
-                  , int nShowCmd ) {
-  parseConfig();
+INT WINAPI WinMain( _In_ HINSTANCE hInstance
+                  , _In_opt_ HINSTANCE hPrevInstance
+                  , _In_ LPSTR lpCmdLine
+                  , _In_ int nShowCmd ) {
+
+  parseConfigFile();
 
   // Turn on Num Lock
   if (!(GetKeyState(VK_NUMLOCK) & 0x0001)) {
@@ -160,7 +161,7 @@ INT WINAPI WinMain( HINSTANCE hInstance
     keybd_event(VK_NUMLOCK, 0x45, KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP, 0);
   }
 
-  HANDLE hHandle = CreateMutexA( NULL, TRUE, MUTEX_NAME );
+  HANDLE hHandle = CreateMutexA(NULL, TRUE, MUTEX_NAME);
   if(ERROR_ALREADY_EXISTS == GetLastError()) {
     return 1;
   }
@@ -171,7 +172,10 @@ INT WINAPI WinMain( HINSTANCE hInstance
     keybd_event(TOGGLE_KEY, 0x45, KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP, 0);
   }
 
-  KEYBOARD_HOOK = SetWindowsHookEx(WH_KEYBOARD_LL, KeyboardCallback, NULL, 0);
+  KEYBOARD_HOOK = SetWindowsHookExA( WH_KEYBOARD_LL
+                                   , KeyboardCallback
+                                   , NULL
+                                   , 0 );
 
   INPUT_DOWN.type           = INPUT_UP.type           = INPUT_MOUSE;
   INPUT_DOWN.mi.dwExtraInfo = INPUT_UP.mi.dwExtraInfo = 0;
@@ -181,14 +185,15 @@ INT WINAPI WinMain( HINSTANCE hInstance
 
   BOOL bRet; 
   MSG msg;
-  while( (bRet = GetMessage( &msg, NULL, WM_KEYFIRST, WM_KEYLAST )) != 0 ) {
+  while( ( bRet = GetMessageA(&msg, NULL, WM_KEYFIRST, WM_KEYLAST) ) != 0 ) {
     if (bRet != -1)  {
       TranslateMessage(&msg);
-      DispatchMessage(&msg);
+      DispatchMessageA(&msg);
     }
   }
 
   UnhookWindowsHookEx(KEYBOARD_HOOK);
+  free(CONFIG_KEYS);
 
   // Turn off Scroll Lock
   if (GetKeyState(TOGGLE_KEY) & 0x0001) {
@@ -196,8 +201,10 @@ INT WINAPI WinMain( HINSTANCE hInstance
     keybd_event(TOGGLE_KEY, 0x45, KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP, 0);
   }
 
-  CloseHandle( hHandle );
-  ReleaseMutex( hHandle );
+  if (hHandle) {
+    CloseHandle(hHandle);
+    ReleaseMutex(hHandle);
+  }
 
   return msg.wParam;
 }
