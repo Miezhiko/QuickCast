1c89bf30da6484a5dd4c28c07cdc3fc9eb1c946c
Miezhiko <Miezhiko@gmail.com>
2023-02-25 16:57:29 +0400

    fix random sitation with CAPS LOCK!

--- src/hotkeys.h
+++ src/hotkeys.h
@@ -83,7 +83,8 @@ LRESULT CALLBACK KeyboardCallback( INT uMsg
             return 1;
           } else break;
         case VK_CAPITAL:
-          if (HOTKEYS_ON)
+          if (HOTKEYS_ON) {
+            static TURNING_CAPSLOCK_OFF = FALSE;
             if (GetKeyState( VK_CONTROL ) & 0x8000) {
               if (GetCursorPos(&CURSOR_POSITION))
                 STORED_CURSOR_POSITION = CURSOR_POSITION;
@@ -95,8 +96,19 @@ LRESULT CALLBACK KeyboardCallback( INT uMsg
                 puts("Custom meme macros Disabled");
               }
               #endif
-            } else if (CUSTOM_MACROS)
+            } else if (CUSTOM_MACROS && !TURNING_CAPSLOCK_OFF) {
               sillyWalkLOL();
+            }
+            if (TURNING_CAPSLOCK_OFF) {
+              break;
+            } else if (GetKeyState(VK_CAPITAL) & 0x0001) {
+              TURNING_CAPSLOCK_OFF = TRUE;
+              keybd_event(VK_CAPITAL, 0x45, KEYEVENTF_EXTENDEDKEY | 0, 0);
+              keybd_event(VK_CAPITAL, 0x45, KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP, 0);
+            } else {
+              TURNING_CAPSLOCK_OFF = FALSE;
+            }
+          }
           return 1;
         case VK_OEM_4:    // [
           if (HOTKEYS_ON && CUSTOM_MACROS) {
